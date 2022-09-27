238f4adb30f83cd9af37d22b0124fa45f5f345ca
PeterHuang <peterhuang@inquartik.com>
2022-09-27 09:08:31 +0800

    fix: height

--- js/custom-swiper.js
+++ js/custom-swiper.js
@@ -43,10 +43,10 @@ var swiper = new Swiper("#swiper_exclusive_mobile", {
   centeredSlides: true,
   loop: true,
   speed: 1500,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
